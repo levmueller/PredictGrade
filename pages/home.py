40db316abddd8360470e777486561a9ec0dac417
carendsen <collinarendsen@gmail.com>
2024-12-05 07:57:33 +0800

    Create home.py

---
+++ pages/home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from joblib import load
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+
+# Session state to track responses
+if 'responses' not in st.session_state:
+    st.session_state.responses = []
+
+
+st.title("Welcome to GradeBoost! 🚀")
+st.header("Assess and boost your semester performance")
+st.markdown("""
+*Dear Student,*
+*We warmly welcome you to our website. Enter relevant factors and calculate your provisional grade for the semester.*
+*Good luck! 🍀*
+""")
