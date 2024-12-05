8436a95148ac8052b29f56750177f289693c5708
Aline Muff <aline.muff@bluewin.ch>
2024-12-05 13:43:12 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ st.markdown("""
 *We warmly welcome you to our website. This website is designed for students aged 15 to 18 and their teachers living in Switzerland.*
 *GradeBoost helps you identify areas where performance may be below average compared to others and provides valuable insights*
 *and personalised tips to address these challenges.*
-Our mission is to guide you in continuously improving your performance and achieving a successful semester.*
+*Our mission is to guide you in continuously improving your performance and achieving a successful semester.*
 *Good luck! 🍀*
 """)
 
