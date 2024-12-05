b7e222d43bd03e22729afe6a045bf432245d9166
Aline Muff <aline.muff@bluewin.ch>
2024-12-05 11:12:18 +0100

    Update Questionnaire.py
    specific question/Disclaimer

--- pages/Questionnaire.py
+++ pages/Questionnaire.py
@@ -5,11 +5,13 @@ st.set_page_config(page_title="Questionnaire", layout="wide")
 
 st.title("Questionnaire")
 st.write("To calculate your grade for the upcoming semester, please complete the questionnaire.")
-st.write("The questionnaire is divided into the following four sections: Personal Information, Academic Information, Activities, Parental Support & Education.")
+st.write("The questionnaire is divided into the following four sections:")
 st.write(" - Personal Information")
 st.write(" - Academic Information")
 st.write(" - Activities")
 st.write(" - Parental Support & Education.")
+
+st.write(" Filling out the questionnaire will take approximately 5 to 10 minutes.")
 
 st.markdown("---")
 
@@ -23,19 +25,19 @@ age = st.slider("2. How old are you?", 15, 18, 15)
 
 # Academic Information
 st.subheader("Academic Information")
-average_time = st.slider("3. How many hours per week do you study on average?", 0, 25, 0)
-absences = st.slider("4. How many days were you absent?", 0, 30, 0)
+average_time = st.slider("3. On average, how many hours per week do you study outside of the classroom? Please use the last 6 months of school as a reference."), 0, 25, 0)
+absences = st.slider("4. How many days were you absent from school in the last 6 months?", 0, 30, 0)
 
-tutoring = st.radio("5. Have you received tutoring?", ["No", "Yes"])
+tutoring = st.radio("5. Have you received any tutoring in the last 6 months? Parental support is not included", ["No", "Yes"])
 tutoring_mapping = {"No": 0, "Yes": 1}
 tutoring_numeric = tutoring_mapping[tutoring]
 
-performance = st.slider("6. What is your current GPA?", min_value=1.0, max_value=6.0, step=0.05)
+performance = st.slider("6. What is your current swiss Grade? You can use your average grade from the last semester as a reference.", min_value=1.0, max_value=6.0, step=0.05)
 
-# Extracurricular Activities
+# Activities
 st.subheader("Extracurricular Activities")
 activities = st.multiselect(
-    "7. Which activities do you participate in?",
+    "7. Which activities do you participate in? You can choose up to 4",
     ["Sports", "Music", "Volunteering", "Other Extracurricular Activities (Theatre, Arts, etc.)"]
 )
 sports = int("Sports" in activities)
@@ -50,8 +52,8 @@ support_mapping = {"No support": 0, "Low": 1, "Moderate": 2, "High": 3, "Very hi
 support_numeric = support_mapping[support]
 
 parental_degree = st.radio(
-    "9. What is the highest education level your parents completed?", 
-    ["No degree", "High School", "Bachelor's", "Master's", "PhD"]
+    "9. What is the highest education level your parents completed? If your parents have different levels of education, please indicate the higher level of education", 
+    ["No degree", "High School/Apprenticeship", "Bachelor's", "Master's", "PhD"]
 )
 degree_mapping = {"No degree": 0, "High School": 1, "Bachelor's": 2, "Master's": 3, "PhD": 4}
 parental_degree_numeric = degree_mapping[parental_degree]
